f4a8aab7076a2c16b42115a88e0da017b8c30e5e
33b3ziz <egyptianhack325@gmail.com>
2023-12-07 17:19:49 +0200

    first commit

---
+++ Introduction And Variables/tasks.py
@@ -0,0 +1,15 @@
+# 1
+name = "Mohamed"
+age = "21"
+country = "Egypt"
+
+# 2
+print("Name:", name + "\nAge:", age + "\nCountry:", country + "\n")
+
+# 3
+print(f"Hello, My Name is {name} And Iam {age} Years Old and I Live In {country}")
+
+# 4
+print(type(name))
+print(type(age))
+print(type(country))
