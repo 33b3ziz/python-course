e9fad149dd97ef2ec73eb859774d120505c1f8eb
33b3ziz <egyptianhack325@gmail.com>
2023-12-13 13:47:14 +0200

    Add number and arithmetic operator examples

---
+++ Numbers & Arithmetic Operators/tasks.py
@@ -0,0 +1,39 @@
+# 1
+print(type(10))
+print(type(10.0))
+print(type(10+3.14j))
+
+# 2
+1+2j
+
+print(1+2j.imag)
+print(1+2j.real)
+
+# Print Imaginary Part Here
+# Print Real Part Here
+
+# 3
+num = 10
+
+print("{:.10f}".format(num))
+
+
+# Needed Ouput
+# 10.0000000000
+
+# 4
+
+num = 159.650
+print(int(num))
+print(type(int(num)))
+
+# Needed Output
+# 159
+# <class 'int'>
+
+# 5
+print(100 - 115) # -15
+print(50 * 30)  # 1500
+print(21 % 4) # 1
+print(110 // 11) # 10
+print(97 // 20) # 4
